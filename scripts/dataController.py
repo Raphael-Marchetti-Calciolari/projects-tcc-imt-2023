8285368382f8b4b3460f3bd890afbed669f4e8c2
raphazik4 <rapha.marchetti@gmail.com>
2023-06-27 11:05:35 -0300

    Atualizado modelo controlador de dados
    - Corrigido scaler
    - Adicionado método para extrair dados normalizados em colunas X e y para ser utilizado no treinamento de modelos

--- scripts/dataController.py
+++ scripts/dataController.py
@@ -8,9 +8,17 @@ class DataController:
         self.raw_df = pd.read_csv(self._path, delimiter=';', on_bad_lines='skip')
         self.df = self.raw_df.dropna().drop_duplicates()
         self.df_columns = list(self.raw_df.columns)
+        self._scaler.fit_transform(self.df)
         self.df_norm = self.normalize_df(self.df)
 
     def normalize_df(self, df:pd.DataFrame) -> pd.DataFrame:
         columns = list(df.columns)
-        norm_data = self._scaler.fit_transform(df)
+        norm_data = self._scaler.transform(df)
         return pd.DataFrame(norm_data, columns=columns)
+    
+    def get_X_y_datasets(self, interest_column:str, columns_to_drop:str=None):
+        if columns_to_drop: columns_to_drop.append(interest_column)
+        else: columns_to_drop = interest_column
+        X = self.df_norm.drop(columns=columns_to_drop).reset_index(drop=True)
+        y = self.df[interest_column]
+        return X, y
